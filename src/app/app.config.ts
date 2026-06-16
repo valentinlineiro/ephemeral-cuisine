e2b2067c52202f60f7c460c428f887f540625735
Valentín Liñeiro <valentinlineiro@gmail.com>
2026-06-16 16:02:13 +0200

    feat: add i18n with ngx-translate (es/en)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -9,5 +10,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-  ]
+    provideTranslateService({
+      lang: 'es',
+    }),
+    provideTranslateHttpLoader(),
+  ],
 };
